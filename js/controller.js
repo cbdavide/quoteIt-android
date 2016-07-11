282a3d8993a6b6290be02db014221245e210e336
David Castelblanco B <david.castelblanco@hotmail.com>
2016-07-11 16:57:09 -0500

    Created the controller of the project that works as an interface of the model and for fetch the quotes from the forismatic api

---
+++ js/controller.js
@@ -0,0 +1,41 @@
+'use strict';
+
+import model from './model';
+
+let route = 'http://api.forismatic.com/api/1.0/?',
+    options = 'method=getQuote&lang=en&format=json';
+
+export default {
+  /*
+   * Fetch the quote from the forismatic api
+   * returns a promise which resolves with the quote(json)
+   */
+  downloadQuote: function() {
+    return new Promise((reject, resolve) => {
+
+      fetch(route + options)
+        .then(res => res.json())
+        .then(json => {
+          resolve(json);
+        })
+        .catch(err => {
+          reject(err);
+        })
+    })
+  },
+  /*
+   * Get all the quotes saved in the device
+   * returns a promise which resolves with an array of quotes
+   */
+  getQuotes: function() {
+    return model.getAllQuotes();
+  },
+  /*
+   * Save a quote in the device
+   * return void
+   */
+  saveQuote: function(quote) {
+    model.saveQuote(quote);
+  }
+
+};
